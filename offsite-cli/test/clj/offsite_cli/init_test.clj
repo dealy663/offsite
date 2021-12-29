ee47a31c77f6b2e14a076e7726991ed597d553f2
Derek <dealy663@gmail.com>
2021-12-28 20:45:31 -0600

    added a few more tests for init, examined the inner parts of the EDN validating the path and the exclude entries

--- offsite-cli/test/clj/offsite_cli/init_test.clj
+++ offsite-cli/test/clj/offsite_cli/init_test.clj
@@ -2,7 +2,22 @@
   (:require [clojure.test :refer :all]
             [offsite-cli.init :refer :all]))
 
+(def test-configs-dir "test/configurations")
+
 (deftest test-init
   (testing "Found default backup-paths"
     (is (not (nil? (get-paths))))
-    (is (nil? (get-paths "non-existing")))))
+    (is (nil? (get-paths "non-existing")))
+
+    (let [short-paths (get-paths (str test-configs-dir "/short-backup-paths.edn"))]
+      (is (not (nil? short-paths)))
+      (is (vector? short-paths))
+      (is (= 1 (count short-paths)))
+      (is (= "test/backup-data/active/du.out" (-> short-paths first :path))))
+
+    (let [paths       (get-paths (str test-configs-dir "/backup-paths.edn"))
+          second-path (second paths)]
+      (is (= "test/backup-data/active/music" (:path second-path)))
+      (let [excluded (:exclude second-path)]
+        (is (vector? excluded))
+        (is (.contains excluded "medium"))))))
