d1a6cb321ac123cc4a37a9f5b08bec0930d11e3c
Derek <dealy663@gmail.com>
2022-02-02 18:00:38 -0800

    added tests for full paths from root

--- offsite-cli/test/clj/offsite_cli/init_test.clj
+++ offsite-cli/test/clj/offsite_cli/init_test.clj
@@ -35,6 +35,7 @@
           (str "The exclusion vector: " exclusions " is missing: " exc1))
       (is (= exc2 (some #{exc2} exclusions))
           (str "The exclusion vector: " exclusions " is missing: " exc2))))
+
   (testing "negative tests for building the relative exclude paths"
     (let [backup-root  {:path (str test-backup-data "/music")}
           excl-none    (build-exclusions backup-root)
@@ -42,7 +43,7 @@
           excl-empty   (build-exclusions backup-root)
           backup-root  {:path (str test-backup-data "/music") :exclusions [""]}
           excl-no-str  (build-exclusions backup-root)
-          backup-root  {:path (str test-backup-data "/music") :exclusions ["" "small2"]}
+          backup-root  {:path (str test-backup-data "/music") :exclusions ["" "small2" " \t"]}
           excl-small2  (build-exclusions backup-root)
           small-dir    (->> "music/small2" (fs/file test-backup-data) fs/canonicalize str)]
       (is (nil? excl-none)
@@ -51,7 +52,20 @@
           "nil should be returned when there are no exclusions")
       (is (nil? excl-no-str)
           "nil should be returned when there are no exclusions")
-      (is (= 1 (count excl-small2))
-          "There should should be only one exclusion and the empty string should be ignored")
+      (is (= 1 (count excl-small2))          "There should should be only one exclusion and the empty string should be ignored")
       (is (= small-dir (some #{small-dir} excl-small2))
-          (str "The exclusion vector: " excl-small2 " is missing: " excl-small2)))))
+          (str "The exclusion vector: " excl-small2 " is missing: " excl-small2))))
+
+  (testing "Fully qualified exclude paths"
+    (let [music-dir    (fs/file test-backup-data "music")
+          music-path   (fs/canonicalize music-dir)
+          path-medium  (str music-path "/medium")
+          path-small   (str music-path "/small")
+          backup-root  {:path (str (fs/path music-dir)) :exclusions [path-medium path-small]}
+          exclusions   (build-exclusions backup-root)]
+      (is (= 2 (count exclusions))
+          "There should only be 2 qualified paths for the small and medium sub directories")
+      (is (= path-small (some #{path-small} exclusions))
+          (str "The exclusion vector: " exclusions " is missing: " path-small))
+      (is (= path-medium (some #{path-medium} exclusions))
+          (str "The exclusion vector: " exclusions " is missing: " path-medium)))))
